f267f70d63b723fdbe9b56ea21d68c2884b42cbe
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-10-09 18:36:57 +0530

    Updated env name.

--- centos/install.sh
+++ centos/install.sh
@@ -11,14 +11,14 @@ export ARCH=${ARCH:="amd64"}
 export TRAEFIK_DOWNLOAD="https://github.com/containous/traefik/releases/download/${VERSION}/traefik_linux-${ARCH}"
 export TRAEFIK_CONFIG_DIR="/etc/traefik"
 export TRAEFIK_CONFIG_FILE="${TRAEFIK_CONFIG_DIR}/traefik.toml"
-export TRAEFIX_SAMPLE_CONFIG="https://raw.githubusercontent.com/containous/traefik/master/traefik.sample.toml"
+export TRAEFIK_SAMPLE_CONFIG_DOWNLOAD="https://raw.githubusercontent.com/containous/traefik/master/traefik.sample.toml"
 
 # Install necessary packages
 yum -y install ${INSTALL_PKGS} ${INSTALL_PKGS_TMP};
 
 # Install software from github releases
 wget -O /usr/local/bin/traefik ${TRAEFIK_DOWNLOAD};
-wget -O ${TRAEFIK_CONFIG_FILE} ${TRAEFIX_SAMPLE_CONFIG};
+wget -O ${TRAEFIK_CONFIG_FILE} ${TRAEFIK_SAMPLE_CONFIG_DOWNLOAD};
 chmod +x /usr/local/bin/traefik;
 
 # Cleanup
